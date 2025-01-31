aa479346d5b5a2e14a17bc3b82aa4d20c30df4ca
zi-han28 <xu_zihan@outlook.com>
2025-01-31 14:29:48 +0800

    First official commit

--- index.js
+++ index.js
@@ -37,29 +37,6 @@ global.db = new sqlite3.Database('./database.db',function(err){
 const organiserRoutes = require('./routes/organiser');
 app.use('/organiser', organiserRoutes);
 
- // Edit Event Routes
- // Path Prefix: /organiser/editEvent
- // Purpose: Handles routes for creating and editing events
-const editEventRoutes = require('./routes/editEvent');
-app.use('/organiser/editEvent', editEventRoutes);
-
- // Settings Routes
- // Path Prefix: /settings
- // Purpose: Handles routes for updating site settings
-const settingsRoutes = require('./routes/settings');
-app.use('/settings', settingsRoutes);
-
- // Attendee Routes
- // Path Prefix: /attendee
- // Purpose: Handles all routes related to attendees, such as viewing events and making bookings.
-const attendeeRoutes = require('./routes/attendee');
-app.use('/attendee', attendeeRoutes);
-
- // Edit Attendee Routes
- // Path Prefix: /attendee/event
- // Purpose: Handles routes for attendees to view and book specific events.
-const editAttendeeRoutes = require('./routes/editattendee');
-app.use('/attendee/event', editAttendeeRoutes);
 
 
 

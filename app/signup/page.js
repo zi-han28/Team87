df5b0076fc586784ac1e6f46e60d1c1886a2f77e
zi-han28 <xu_zihan@outlook.com>
2025-03-01 01:12:12 +0800

    cleaned up code

---
+++ app/signup/page.js
@@ -0,0 +1,160 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import Link from "next/link"; // Next.js Link component
+import { NextResponse } from 'next/server';
+
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null); // Track error messages
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const userData = { email, username, password };
+
+    try {
+      const response = await fetch("/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.message); // "User created successfully"
+        setEmail("");
+        setUsername("");
+        setPassword("");
+        setError(null);
+        setShowAlert(false);
+        window.location.href = "/profile";
+      } else if(data.message == "User already exists"){
+          setShowAlert(true);
+      } 
+      else {
+        setError(data.message || "Something went wrong. Please try again.");
+      }
+    } catch (err) {
+      setError("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className=" min-h-screen flex items-center justify-center">
+      {/* Show alert for "Account already exists" */}
+      {showAlert && (
+        <div
+          className="absolute top-[200px] flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+          role="alert">
+          <svg
+            className="shrink-0 w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+            <div className="ms-3 text-sm font-medium">
+              Account already exists.{' '}
+              <Link href="/login" className="font-semibold underline hover:no-underline">
+                  Try logging in.
+              </Link>
+            </div>
+          <button
+            type="button"
+            className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+            onClick={() => setShowAlert(false)} // Close the alert
+            aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
+      <div className="bg-[#613DC1] text-white py-10 px-12 rounded-2xl shadow-xl w-[500px] md:w-[600px]">
+      <span className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">Sign Up</h2>
+        </span>
+      {/* sign up form */}
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form  className="space-y-6" onSubmit={handleSubmit} >
+          {/* email section */}
+          <div>
+            <label htmlFor="email" className="block text-sm/6 font-medium"> Email</label>
+            <div className="mt-2">
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                required/>
+            </div>
+          </div>
+          {/* username section */}
+          <div>
+          <div className="flex items-center justify-between">
+            <label className="block text-sm/6 font-medium" htmlFor="username">Username</label>
+            </div>
+            <div className="mt-2">
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              required/>
+          </div>
+          </div>
+          {/* password section */}
+          <div>
+          <div className="flex items-center justify-between">
+            <label
+              className="block text-sm/6 font-medium"
+              htmlFor="password">Password</label>
+              </div>
+            <div className="mt-2">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              required
+            />
+          </div>
+          </div>
+          {/* submit button */}
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  );
+};

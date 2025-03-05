c526774e9a585588a72d0cb096ea7fa937f535d3
zi-han28 <xu_zihan@outlook.com>
2025-03-05 15:41:25 +0800

    comments to user context

---
+++ components/UserContext.js
@@ -0,0 +1,22 @@
+// components/UserContext.js
+"use client";
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+//set defauly user global stauts
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    user_id: null,
+    user_username: null,
+    user_email: null,
+  });
+// wrap user global status 
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
